df624b71939ea76680446d3d93055920b89d6a30
Danniel Libor <dlibor.dev@gmail.com>
2025-03-31 13:40:58 +0800

    cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -9,4 +9,15 @@ class Cart extends Model
 {
     /** @use HasFactory<\Database\Factories\CartFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'quantity'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
--- database/factories/CartFactory.php
+++ database/factories/CartFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,9 @@ class CartFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => fake()->randomDigit(),
+            'product_id' => Product::factory(),
+            'quantity' => fake()->randomDigit()
         ];
     }
 }
